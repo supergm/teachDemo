6607608160a15e12d34d84bb6ea9e52950b4e921
GMroot <31339100+GMroot@users.noreply.github.com>
2017-08-25 15:51:48 +0800

    复制一波

--- admin.php
+++ admin.php
@@ -20,6 +20,14 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',True);
 
 define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
+define('BIND_MODULE', 'Admin');
 
 // 定义应用目录
 define('APP_PATH','./Application/');
